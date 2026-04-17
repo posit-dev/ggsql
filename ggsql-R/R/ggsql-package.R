9112e6250027e81dd39e6d7c4dcfd2cded069629
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-04-17 11:36:13 +0200

    remove package

--- ggsql-R/R/ggsql-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @import rlang
-#' @importFrom R6 R6Class
-#' @useDynLib ggsql, .registration = TRUE
-## usethis namespace: end
-NULL
