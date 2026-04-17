df413372e84296879bbd88fb9a023a978d2b9ffe
George Stagg <george.stagg@posit.co>
2026-04-17 17:39:04 +0100

    Bump to version 0.2.2

--- tree-sitter-ggsql/bindings/python/__init__.py
+++ tree-sitter-ggsql/bindings/python/__init__.py
@@ -31,5 +31,5 @@ except ImportError:
         except Exception as e:
             raise ImportError(f"Could not load tree-sitter-ggsql language: {e}")
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __all__ = ["language"]
