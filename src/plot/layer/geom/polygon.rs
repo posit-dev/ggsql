56780b0df397ebf2a6ff00ca8674d2c32ed3fcda
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-04-28 16:57:49 +0200

    polygon and path doesn't allow aggregation

--- src/plot/layer/geom/polygon.rs
+++ src/plot/layer/geom/polygon.rs
@@ -37,14 +37,6 @@ impl GeomTrait for Polygon {
         }];
         PARAMS
     }
-
-    fn supports_aggregate(&self) -> bool {
-        true
-    }
-
-    fn aggregate_slots(&self) -> &'static [u8] {
-        &[1, 2]
-    }
 }
 
 impl std::fmt::Display for Polygon {
