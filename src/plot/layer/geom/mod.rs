ee499981950b320f7fe8bb7428df4f5d68f2e320
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-05-07 16:18:17 +0200

    appease our dear lord and master clippy

--- src/plot/layer/geom/mod.rs
+++ src/plot/layer/geom/mod.rs
@@ -305,10 +305,7 @@ pub trait GeomTrait: std::fmt::Debug + std::fmt::Display + Send + Sync {
 
 /// True when `parameters["aggregate"]` is set to a non-null string or array.
 pub(crate) fn has_aggregate_param(parameters: &HashMap<String, ParameterValue>) -> bool {
-    match parameters.get("aggregate") {
-        Some(ParameterValue::String(_)) | Some(ParameterValue::Array(_)) => true,
-        _ => false,
-    }
+    matches!(parameters.get("aggregate"), Some(ParameterValue::String(_)) | Some(ParameterValue::Array(_)))
 }
 
 /// Wrapper struct for geom trait objects
